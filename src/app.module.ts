14f83ab8aa934bb5f7d62ecfda0f2b8c062be620
Sandeep Kumar <sandepten@gmail.com>
2025-03-10 15:45:44 +0530

    add: injestion apis

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { DocumentsModule } from './documents/documents.module';
+import { IngestionModule } from './ingestion/ingestion.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule, DocumentsModule],
+  imports: [AuthModule, PrismaModule, DocumentsModule, IngestionModule],
   controllers: [AppController],
   providers: [AppService],
 })
